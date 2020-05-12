f3fce79bdbfbca0613cb3c63ee95212b039623df
Agaba Andrew <agabaandre@gmail.com>
2020-05-12 21:37:37 +0300

    cleaned header

--- assets/engine/header.php
+++ assets/engine/header.php
@@ -2,54 +2,30 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Attendance Tracking System</title>
-    <!-- Tell the browser to be responsive to screen width -->
-	<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" type="image/x-icon">
+	  <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" type="image/x-icon">
     <link rel="icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" type="image/x-icon">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-		 	
-   	 <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-
-    <!-- DataTables -->
-    <!-- Bootstrap 3.3.5 -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/select2/select2.min.css">
-	    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+   	<script src="<?php echo base_url(); ?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+	  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/select2/select2.min.css">
+	  <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/icons.min.css">
- 
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/skins-green.min.css">
-    <!-- iCheck -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/flat/blue.css">
-    <!-- Morris chart -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/morris/morris.css">
-    <!-- jvectormap -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-    <!-- Date Picker -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datepicker/datepicker3.css">
-    <!-- Select2 -->
-	<!-- bootstrap time picker -->
     <script src="<?php echo base_url(); ?>assets/plugins/timepicker/bootstrap-timepicker.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/lib/jquery-1.10.2.min.js"></script> 
-  <!-- Daterange picker -->
+   	<script src="<?php echo base_url(); ?>assets/lib/jquery-1.10.2.min.js"></script> 
     <script src="<?php echo base_url(); ?>assets/plugins/select2/select2.full.min.js"></script>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker-bs3.css">
-    <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-	<!--data tables -->
-
-	<script src="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+	  <script src="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
-	 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.css">
-    <!-- SlimScroll -->
-    <!-- FastClick -->
+	  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.css">
     <script src="<?php echo base_url(); ?>assets/plugins/fastclick/fastclick.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
     <script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
-	
-    <!-- page script -->
    <script>
       $(function() {
         $('#mydata').DataTable({
@@ -59,84 +35,27 @@
           "ordering": true,
           "info": true,
           "autoWidth": true,
-		  "responsive": true,
-		  "iDisplayLength": 20,
+          "responsive": true,
+          "iDisplayLength": 20,
     	  "aLengthMenu": [[10, 20, 30, 50, 100, -1], [10, 20, 30, 50, 100, "All"]]
         });
-      });
-    </script>
-	  <script>
-    $(function(){
-      // turn the element to select2 select style
-      $('.select2').select2();
-    });
-  </script>
-	
-    <!-- Font Awesome --
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons --
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- DataTables -->
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.css">	
-    <!-- jQuery UI 1.11.4 --
-    <script src="<?php echo base_url(); ?>assets/https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-    $.widget.bridge('uibutton', $.ui.button);
+        });
 
-	  //Timepicker
-    $(".timepicker").timepicker({
+        $(function(){
+        $('.select2').select2();
+        });
+
+      $.widget.bridge('uibutton', $.ui.button);r
+       $(".timepicker").timepicker({
       showInputs: false
     });
-    </script>
-	
-    <!-- Morris.js charts --
-    <script src="<?php echo base_url(); ?>assets/https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
-    <script src="<?php echo base_url(); ?>assets/plugins/morris/morris.min.js"></script>
-    <!-- Sparkline -->
-    <script src="<?php echo base_url(); ?>assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!-- jvectormap -->
-    <script src="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="<?php echo base_url(); ?>assets/plugins/knob/jquery.knob.js"></script>
-    <!-- daterangepicker --
-    <script src="<?php echo base_url(); ?>assets/https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>-->
+  </script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.css">	
     <script src="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker.js"></script>
-    
-    <!-- datepicker -->
     <script src="<?php echo base_url(); ?>assets/plugins/datepicker/bootstrap-datepicker.js"></script>
-    <!-- Bootstrap WYSIHTML5 -->
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-    <!-- Slimscroll -->
-	
-   <script>
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-	
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-</script>
-	
     <script src="<?php echo base_url(); ?>assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-    <!-- AdminLTE App -->
     <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="<?php echo base_url(); ?>assets/dist/js/pages/dashboard.js"></script>
-       <!--  -->
   </head>
