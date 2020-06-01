cf400876027679a3c60418faa23fc8850f4e2b24
AGABA ANDREW <agabaandre@gmail.com>
2020-06-01 11:08:38 +0300

    error page rendering updated

--- application/view/main.php
+++ application/view/main.php
@@ -1,30 +1,30 @@
 <?php
 
-  if (empty($_SESSION['uuid']))
-	{
-		$baseurl= base_url();
-		header("Location:$baseurl");
-		exit();
-	}
-  ?>
-<!DOCTYPE html >
+if (empty($_SESSION['uuid'])) {
+  $baseurl = base_url();
+  header("Location:$baseurl");
+  exit();
+}
+?>
+<!DOCTYPE html>
 <html class="no-js">
-<?php 
-	  include_once("assets/engine/header.php");
+<?php
+include_once("assets/engine/header.php");
 ?>
-<?php 
-	  include_once("assets/engine/menu.php");
+<?php
+include_once("assets/engine/menu.php");
 ?>
-  <div class="content-wrapper">
-      <section class="content" >
-            <?php
-                  $viewname=$data['template'];
-                  include($viewname.'.php');						
-            ?>						
-      </section>
-  </div>
-<?php 
-	  include_once("assets/engine/footer.php");
+<div class="content-wrapper">
+    <section class="content">
+        <?php
+    $viewname = $data['template'];
+    include($viewname . '.php');
+    ?>
+    </section>
+</div>
+<?php
+include_once("assets/engine/footer.php");
 ?>
 </body>
+
 </html>
